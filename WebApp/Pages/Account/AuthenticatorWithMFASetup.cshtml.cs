ff7a36d1f1bd2b7b985950cd1641d9eda2489841
TomaszLenart <t.lenart234@gmail.com>
2026-01-02 18:33:29 +0100

    Manual authenticator setup

---
+++ WebApp/Pages/Account/AuthenticatorWithMFASetup.cshtml.cs
@@ -0,0 +1,71 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using WebApp.Data.Account;
+
+namespace WebApp.Pages.Account
+{
+    [Authorize]
+    public class AuthenticatorWithMFASetupModel : PageModel
+    {
+        private readonly UserManager<User> _userManager;
+
+        [BindProperty]
+        public SetupMFAViewModel ViewModel { get; set; }
+
+        [BindProperty]
+        public bool Succeeded { get; set; }
+
+        public AuthenticatorWithMFASetupModel(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+            ViewModel = new SetupMFAViewModel();
+            Succeeded = false;
+        }
+
+        public async Task OnGet()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user != null)
+            {
+                await _userManager.ResetAuthenticatorKeyAsync(user);
+                var key = await _userManager.GetAuthenticatorKeyAsync(user);
+                ViewModel.Key = key;
+            }
+
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+            var user = await _userManager.GetUserAsync(User);
+            if (user != null && await _userManager.VerifyTwoFactorTokenAsync( //checks AspNetUserTokens for the 2FA token by user
+                user,
+                _userManager.Options.Tokens.AuthenticatorTokenProvider,
+                ViewModel.SecurityCode))
+            {
+                await _userManager.SetTwoFactorEnabledAsync(user, true);
+                Succeeded = true;
+            }
+            else
+            {
+                ModelState.AddModelError("SetupMFA", "Something went wrong ");
+            }
+
+            return Page();
+        }
+    }
+
+    public class SetupMFAViewModel
+    {
+        public string? Key { get; set; }
+        [Required]
+        [Display(Name = "Code")]
+        public string SecurityCode { get; set; } = string.Empty;
+    }
+}
